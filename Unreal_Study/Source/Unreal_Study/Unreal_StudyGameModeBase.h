a414631865b6aa846c40fb5257aa70642b25e23d
choijiuk1017 <86818323+choijiuk1017@users.noreply.github.com>
2023-03-28 00:37:42 +0900

    첫 생성

---
+++ Unreal_Study/Source/Unreal_Study/Unreal_StudyGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Unreal_StudyGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREAL_STUDY_API AUnreal_StudyGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
