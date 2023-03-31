5dda05a219168fce68f461c6a98258c569842c98
choijiuk1017 <86818323+choijiuk1017@users.noreply.github.com>
2023-04-01 00:32:50 +0900

    플레이어 이동 구현 중
    싯팔 왜 자꾸 안되는거임

--- Unreal_Study/Source/Unreal_Study/PlayerCharacter.cpp
+++ Unreal_Study/Source/Unreal_Study/PlayerCharacter.cpp
@@ -1,100 +1,109 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
 #include "PlayerCharacter.h"
-
 #include "Camera/CameraComponent.h"
 #include "GameFramework/SpringArmComponent.h"
 #include "GameFramework/CharacterMovementComponent.h"
 
-// Sets default values
+// 디폴트값 설정
 APlayerCharacter::APlayerCharacter()
 {
- 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
-	PrimaryActorTick.bCanEverTick = true;
+    // 이 캐릭터가 프레임마다 Tick()을 호출하도록 설정합니다.  이 설정이 필요 없는 경우 비활성화하면 퍼포먼스가 향상됩니다.
+    PrimaryActorTick.bCanEverTick = true;
 
+    // 클래스 컴포넌트 인스턴스화
+    SpringArmComp = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArmComp"));
+    CameraComp = CreateDefaultSubobject<UCameraComponent>(TEXT("CameraComp"));
 
-	// 클래스 컴포넌트 인스턴스화
+    //캐릭터 메시 트랜스폼의 위치와 회전을 설정합니다.
+    GetMesh()->SetRelativeLocationAndRotation(FVector(0.0f, 0.0f, -90.0f), FQuat(FRotator(0.0f, -90.0f, 0.0f)));
 
-	SpringArmComp = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArmComp"));
+    // 클래스 컴포넌트를 디폴트 캐릭터의 스켈레탈 메시 컴포넌트에 어태치합니다.
+    SpringArmComp->SetupAttachment(GetMesh());
+    CameraComp->SetupAttachment(SpringArmComp, USpringArmComponent::SocketName);
 
-	CameraComp = CreateDefaultSubobject<UCameraComponent>(TEXT("CameraComp"));
+    //스프링 암의 클래스 변수를 설정합니다.
+    SpringArmComp->bUsePawnControlRotation = true;
 
-	//캐릭터 메시 트랜스폼의 위치와 회전을 설정합니다.
-
-	GetMesh()->SetRelativeLocationAndRotation(FVector(0.0f, 0.0f, -90.0f), FQuat(FRotator(0.0f, -90.0f, 0.0f)));
-
-	// 클래스 컴포넌트를 디폴트 캐릭터의 스켈레탈 메시 컴포넌트에 어태치합니다.
-
-	SpringArmComp->SetupAttachment(GetMesh());
-
-	CameraComp->SetupAttachment(SpringArmComp, USpringArmComponent::SocketName);
-
-	//스프링 암의 클래스 변수를 설정합니다.
-
-	SpringArmComp->bUsePawnControlRotation = true;
-
-	//캐릭터 무브먼트 컴포넌트의 클래스 변수를 설정합니다.
-
-	GetCharacterMovement()->bOrientRotationToMovement = true;
-
-	GetCharacterMovement()->bUseControllerDesiredRotation = true;
-
-	GetCharacterMovement()->bIgnoreBaseRotation = true;
-
+    //캐릭터 무브먼트 컴포넌트의 클래스 변수를 설정합니다.
+    GetCharacterMovement()->bOrientRotationToMovement = true;
+    GetCharacterMovement()->bUseControllerDesiredRotation = true;
+    GetCharacterMovement()->bIgnoreBaseRotation = true;
 }
 
-// Called when the game starts or when spawned
+// 게임 시작 또는 스폰 시 호출
 void APlayerCharacter::BeginPlay()
 {
-	Super::BeginPlay();
-	
-}
-
-// Called every frame
-void APlayerCharacter::Tick(float DeltaTime)
-{
-	Super::Tick(DeltaTime);
-
-}
-
-// Called to bind functionality to input
-void APlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
-{
-	Super::SetupPlayerInputComponent(PlayerInputComponent);
-
-	PlayerInputComponent->BindAxis("MoveForward", this, &APlayerCharacter::MoveForward);
-	PlayerInputComponent->BindAxis("MoveRight", this, &APlayerCharacter::MoveRight);
-	PlayerInputComponent->BindAxis("Turn", this, &APawn::AddControllerYawInput);
-	PlayerInputComponent->BindAxis("LookUp", this, &APawn::AddControllerPitchInput);
+    Super::BeginPlay();
 
 }
 
 void APlayerCharacter::MoveForward(float AxisValue)
 {
-	if ((Controller != nullptr) && (AxisValue != 0.0f))
-	{
-		// 앞쪽 찾기
-		const FRotator Rotation = Controller->GetControlRotation();
-		const FRotator YawRotation(0, Rotation.Yaw, 0);
+    if ((Controller != nullptr) && (AxisValue != 0.0f))
+    {
+        // 앞쪽 찾기
+        const FRotator Rotation = Controller->GetControlRotation();
+        const FRotator YawRotation(0, Rotation.Yaw, 0);
 
-		// 앞쪽 벡터 구하기
-		const FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
-		AddMovementInput(Direction, AxisValue);
-	}
+        // 앞쪽 벡터 구하기
+        const FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::X);
+        AddMovementInput(Direction, AxisValue);
+    }
 }
 
 void APlayerCharacter::MoveRight(float AxisValue)
 {
-	if ((Controller != nullptr) && (AxisValue != 0.0f))
-	{
-		// 오른쪽 찾기
-		const FRotator Rotation = Controller->GetControlRotation();
-		const FRotator YawRotation(0, Rotation.Yaw, 0);
-		// 오른쪽 벡터 구하기 
-		const FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+    if ((Controller != nullptr) && (AxisValue != 0.0f))
+    {
+        // 오른쪽 찾기
+        const FRotator Rotation = Controller->GetControlRotation();
+        const FRotator YawRotation(0, Rotation.Yaw, 0);
 
-		// 해당 방향으로 이동 추가
-		AddMovementInput(Direction, AxisValue);
-	}
+        // 오른쪽 벡터 구하기 
+        const FVector Direction = FRotationMatrix(YawRotation).GetUnitAxis(EAxis::Y);
+
+        // 해당 방향으로 이동 추가
+        AddMovementInput(Direction, AxisValue);
+    }
+}
+
+void APlayerCharacter::BeginSprint()
+{
+    GetCharacterMovement()->MaxWalkSpeed = 1000.0f;
+}
+
+void APlayerCharacter::EndSprint()
+{
+    GetCharacterMovement()->MaxWalkSpeed = 600.f;
+}
+
+void APlayerCharacter::BeginCrouch()
+{
+    Crouch();
+}
+
+void APlayerCharacter::EndCrouch()
+{
+    UnCrouch();
+}
+
+// 프레임마다 호출
+void APlayerCharacter::Tick(float DeltaTime)
+{
+    Super::Tick(DeltaTime);
+}
+
+//함수 기능을 입력에 바인딩하기 위해 호출
+void APlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+    Super::SetupPlayerInputComponent(PlayerInputComponent);
+    PlayerInputComponent->BindAxis("MoveForward", this, &APlayerCharacter::MoveForward);
+    PlayerInputComponent->BindAxis("MoveRight", this, &APlayerCharacter::MoveRight);
+    PlayerInputComponent->BindAxis("Turn", this, &APawn::AddControllerYawInput);
+    PlayerInputComponent->BindAxis("LookUp", this, &APawn::AddControllerPitchInput);
+    PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &ACharacter::Jump);
+    PlayerInputComponent->BindAction("Jump", IE_Released, this, &ACharacter::StopJumping);
+    PlayerInputComponent->BindAction("Crouch", IE_Pressed, this, &APlayerCharacter::BeginCrouch);
+    PlayerInputComponent->BindAction("Crouch", IE_Released, this, &APlayerCharacter::EndCrouch);
+    PlayerInputComponent->BindAction("Sprint", IE_Pressed, this, &APlayerCharacter::BeginSprint);
+    PlayerInputComponent->BindAction("Sprint", IE_Released, this, &APlayerCharacter::EndSprint);
 }
--- Unreal_Study/Source/Unreal_Study/PlayerCharacter.h
+++ Unreal_Study/Source/Unreal_Study/PlayerCharacter.h
@@ -1,5 +1,3 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
@@ -7,39 +5,50 @@
 #include "PlayerCharacter.generated.h"
 
 UCLASS()
-class UNREAL_STUDY_API APlayerCharacter : public ACharacter
+class CHARACTERMOVEMENT_API APlayerCharacter : public ACharacter
 {
-	GENERATED_BODY()
+    GENERATED_BODY()
 
 public:
-	// Sets default values for this character's properties
-	APlayerCharacter();
+    //이 캐릭터의 프로퍼티에 적용되는 디폴트값 설정
+    APlayerCharacter();
 
 protected:
-	// Called when the game starts or when spawned
-	virtual void BeginPlay() override;
+    // 게임 시작 또는 스폰 시 호출
+    virtual void BeginPlay() override;
 
-	//스프링 암 컴포넌트가 플레이어 뒤에 있는 카메라를 따라갑니다.
-	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
-		class USpringArmComponent* SpringArmComp;
+    //스프링 암 컴포넌트가 플레이어 뒤에 있는 카메라를 따라갑니다.
+    UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+        class USpringArmComponent* SpringArmComp;
 
-	//플레이어가 카메라를 따라갑니다.
-	UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
-		class UCameraComponent* CameraComp;
+    //플레이어가 카메라를 따라갑니다.
+    UPROPERTY(VisibleAnywhere, BlueprintReadWrite)
+        class UCameraComponent* CameraComp;
 
-	//앞쪽/뒤쪽 입력을 위해 호출합니다.
+    //앞쪽/뒤쪽 입력을 위해 호출합니다.
+    void MoveForward(float InputAxis);
 
-	void MoveForward(float InputAxis);
+    //왼쪽/오른쪽 입력을 위해 호출합니다.
+    void MoveRight(float InputAxis);
 
-	//왼쪽/오른쪽 입력을 위해 호출합니다.
+    //캐릭터 무브먼트 속도를 스프린트 값으로 설정합니다.
+    void BeginSprint();
 
-	void MoveRight(float InputAxis);
+    //캐릭터 무브먼트 속도를 디폴트 속도 값으로 다시 설정합니다.
+    void EndSprint();
 
-public:	
-	// Called every frame
-	virtual void Tick(float DeltaTime) override;
+    //캐릭터에게 앉기를 요청합니다.
+    void BeginCrouch();
 
-	// Called to bind functionality to input
-	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+    //캐릭터에게 앉기 종료를 요청합니다.
+    void EndCrouch();
+
+public:
+
+    // 프레임마다 호출
+    virtual void Tick(float DeltaTime) override;
+
+    //함수 기능을 입력에 바인딩하기 위해 호출
+    virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
 
 };
